b3f1c0f5a38b888c2d2b34ca5effdf06543fb10a
kuntvvdakwsyn <alexandrumaruscenco2012@gmail.com>
2026-07-28 16:46:44 +0300

    Add XDG configuration and new packages

--- home.nix
+++ home.nix
@@ -5,4 +5,14 @@
     home.homeDirectory = "/home/lain";
     programs.git.enable = true;
     home.stateVersion = "25.05";
+    xdg.conifFile = {
+        "foot".source = ./dot/foot;
+        "hypr".source = ./dot/hypr;
+        "waybar".source = ./dot/waybar;   
+}
+    home.packages = with pkgs; [
+        foot 
+        waybar
+        hyprpaper
+    ];
 }
