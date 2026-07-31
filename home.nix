c0ebc8f42573997a2cba86c9d91dca39cff4c672
kuntvvdakwsyn <alexandrumaruscenco2012@gmail.com>
2026-07-31 21:17:18 +0300

    Remove xdg configuration and package list
    Removed configuration for xdg.conifFile and home.packages.

--- home.nix
+++ home.nix
@@ -5,16 +5,4 @@
     home.homeDirectory = "/home/lain";
     programs.git.enable = true;
     home.stateVersion = "25.05";
-    xdg.conifFile = {
-        "foot".source = ./dot/foot;
-        "hypr".source = ./dot/hypr;
-        "waybar".source = ./dot/waybar;   
-        "rofi".source = ./dot/rofi;
-}
-    home.packages = with pkgs; [
-        foot 
-        waybar
-        hyprpaper
-        rofi
-    ];
 }
